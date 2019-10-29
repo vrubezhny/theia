8022eb42d948612c15d38c3121581b3810f76b84
Paul Maréchal <paul.marechal@ericsson.com>
2019-10-29 16:15:46 -0400

    wip
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

---
+++ packages/core/src/electron-main/default-electron-behaviour copy.ts
@@ -0,0 +1,68 @@
+/********************************************************************************
+ * Copyright (C) 2019 Ericsson and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import yargs = require('yargs');
+const createYargs: (argv?: string[], cwd?: string) => yargs.Argv = require('yargs/yargs');
+
+import { injectable, inject } from 'inversify';
+import { realpathSync } from 'fs';
+
+import { ElectronApplicationContribution, ElectronApplication, ElectronApplicationSettings, ExecutionParams } from './electron-application';
+import URI from '../common/uri';
+import { resolve } from 'path';
+
+/**
+ * Override this binding to setup your own electron behavior, i.e.: What to do
+ * when a user launches your application.
+ */
+export const ElectronBehavior = Symbol('ElectronBehavior');
+
+@injectable()
+export class DefaultElectronBehavior implements ElectronApplicationContribution {
+
+    @inject(ElectronApplication)
+    protected readonly app: ElectronApplication;
+
+    @inject(ElectronApplicationSettings)
+    protected readonly settings: ElectronApplicationSettings;
+
+    launch(params: ExecutionParams): void {
+        const args = this.createCli(params).argv;
+        const window = this.app.createRawWindow({
+            show: false,
+        });
+        window.on('ready-to-show', () => window.show());
+        const url = new URI()
+            .withScheme('file')
+            .withPath(this.settings.THEIA_FRONTEND_HTML_PATH)
+            .withFragment(realpathSync(resolve(params.cwd, args.file)))
+            .withQuery(`port=${this.app.backendPort}`);
+        window.loadURL(url.toString(true));
+    }
+
+    protected createCli(params: ExecutionParams): yargs.Argv {
+        return createYargs(params.argv, params.cwd)
+            .command('$0 [file]',
+                false,
+                cmd => cmd
+                    .positional('file', {
+                        type: 'string',
+                    }),
+                args => undefined,
+            );
+    }
+
+}
